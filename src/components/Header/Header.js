d83d1297d7480763065339fd0f47d3c7fa7475fb
The-Mechanical-Ocean <alex.kudrin@bk.ru>
2022-10-04 12:18:18 +0300

    doctor-lending-1

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import {Link, useLocation} from 'react-router-dom';
+// import Navigation from "../Navigation/Navigation";
+import './Header.css';
+
+function Header() {
+
+  return (
+    <header className='header'>
+      <div className='header__img-container'>
+        <a href='' className='header__link'><div className='header__face-img'></div></a>
+        <a href='' className='header__link'><div className='header__inst-img'></div></a>
+        <a href='' className='header__link'><div className='header__twit-img'></div></a>
+      </div>
+      <h1 className='header__title'>Центер Доктора Лабунца</h1>
+      <div className='header__menu'></div>
+    </header>
+  );
+};
+
+export default Header;
+
