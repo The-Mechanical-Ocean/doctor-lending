fc29ac4f169be7398dfb28834cabfac266877a78
The-Mechanical-Ocean <alex.kudrin@bk.ru>
2022-10-06 16:39:29 +0300

    prob fix pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import Header from '../Header/Header';
 import AboutProject from '../AboutProject/AboutProject';
@@ -16,15 +15,6 @@ function App() {
       <Privilege />
       <Problem />
       <Inovation />
-      <Routes>
-        {/* <Route path='/' element={<Main />}/>
-        <Route path='/signup' element={<Register />}/>
-        <Route path='/signin' element={<Login />}/>
-        <Route path='/movies' element={<Movies/>}/>
-        <Route path='/saved-movies' element={<SavedMovies/>}/>
-        <Route path='/profile' element={<Profile name={'Александр'} email={'pochta@yandex.ru'}/>}/>
-        <Route path='*' element={<PageNotFound/>}/> */}
-      </Routes>  
       <Footer />      
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './index.css';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import {createRoot} from "react-dom/client";
@@ -10,9 +9,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
 
